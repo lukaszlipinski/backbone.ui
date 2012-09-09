8c0c88fb15ae435ae0cb58ce084a86d4298b7179
Lukasz Lipinski <uzza17@gmail.com>
2012-09-09 15:09:50 +0200

    dsds

--- src/js/backbone.ui.button.js
+++ src/js/backbone.ui.button.js
@@ -205,7 +205,7 @@
 	/**
 	 * **Description**
 	 *
-	 * Backbone.UI.Button component extends standard functionality of link or button elements. 1234567
+	 * Backbone.UI.Button component extends standard functionality of link or button elements. 123
 	 *
 	 * **Additional information**
 	 *
